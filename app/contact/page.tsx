6d0e6bb4988c0991f782e4a68bd931222f5fe480
Alonso <alonsoortiz73@gmail.com>
2023-12-24 20:22:27 -0800

    Send emails using mailto URI

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,12 @@
+import ContactForm from "@/components/contact-form";
 
 export default function Blog() {
-    return (
-      <main className="flex min-h-full flex-col items-center justify-between p-24">
-      Contact
-      </main>
-    )
-  }
-  
+  return (
+    <main className="flex flex-col items-center justify-between min-h-full p-4">
+      <h1 className="py-8 text-4xl font-bold">Message Form</h1>
+      <div>
+        <ContactForm />
+      </div>
+    </main>
+  )
+}
---
+++ components/contact-form.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "./ui/textarea";
+
+const formSchema = z.object({
+    subject: z.string().min(2, {
+        message: "Subject must be at least 2 characters.",
+    }),
+    body: z.string().min(2, {
+        message: "Message must be at least 2 characters.",
+    }),
+});
+
+export default function ContactForm() {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            subject: "",
+            body: "",
+        },
+    });
+
+    const onSubmit = (data: { subject: string, body: string }) => {
+        const subjectEndcoded = encodeURIComponent(data.subject);
+        const bodyEncoded = encodeURIComponent(data.body);
+        window.location.href = `mailto:aortiz.dev@outlook.com?subject=${subjectEndcoded}&body=${bodyEncoded}`;
+    };
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="subject"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Subject</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Inquiry" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+
+                />
+                <FormField
+                    control={form.control}
+                    name="body"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Body</FormLabel>
+                            <FormControl>
+                                <Textarea placeholder="Enter your message here" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <div className="text-center"><Button type="submit">Submit</Button></div>
+            </form>
+        </Form>
+    );
+
+}
