9c13fbbe5fa285ef3d57e0c8d702e7b2e6c88b3f
alonso-ov <103380838+alonso-ov@users.noreply.github.com>
2024-08-21 12:30:26 -0700

    Update color of icon links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,10 +8,10 @@ export default function ContactPage() {
       <h1 className="py-8 text-4xl font-bold text-center">Feel free to Connect!</h1>
       <div className="flex flex-col space-y-8 sm:space-y-0 sm:flex-row sm:space-x-8">
         <Link href="https://www.linkedin.com/in/alonso-ov/">
-          <Linkedin size={42} />
+          <Linkedin size={42} color="#2563EB" />
         </Link>
         <Link href="https://github.com/alonso-ov">
-          <Github size={42} />
+          <Github size={42} color="#2563EB" />
         </Link>
       </div>
       <span className="w-2/3 my-12 border-b sm:w-1/3"></span>
